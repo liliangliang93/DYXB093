d33f46c95c2abdca156672aa6d71c6ef8bb06236
子永 <ziyong@ziyongdemacbook-pro.local>
2017-10-30 21:36:35 +0800

    初始化目录

---
+++ DYZB/DYZB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DYZB
+//
+//  Created by 子永 on 17/10/30.
+//  Copyright © 2017年 Dan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
